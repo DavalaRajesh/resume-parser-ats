90eb801d1c68ea9791e6873a71217128723c086a
Davala Rajesh <davalarajesh299@gmail.com>
2026-07-16 15:34:25 +0530

    Use DATABASE_URL for Render

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 
 class Config:
-
     SECRET_KEY = "resume-parser"
 
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:0957@localhost:9057/resume_parser"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://resume_parser_wqqs_user:kN0dXLneYlj2AfRBIUK4QC6e8Eehv4Jm@dpg-d9can2m7r5hc73eu93og-a/resume_parser_wqqs"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
